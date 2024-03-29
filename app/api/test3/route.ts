9876c0581309cfcbb48344c3f01490153127d6c7
洪健翔 Hung, Chien-Hsiang <abcdefg2981@gmail.com>
2024-03-29 07:15:24 +0000

    pexel succeed

--- app/api/test3/route.ts
+++ app/api/test3/route.ts
@@ -1,6 +1,15 @@
+import { createClient } from 'pexels';
+
 export async function GET(request: Request) {
-    console.log('Called successfully!');
-    return new Response('Called successfully!', {
+
+    const client = createClient(process.env.PEXELS_API_KEY!);
+    const query = 'Nature';
+    const photos_object = await client.photos.search({query, per_page: 1});
+    const photos_json = await JSON.parse(JSON.stringify(photos_object));
+
+    console.log(photos_json.photos[0].src.tiny);
+
+    return new Response(photos_json.photos[0].src.tiny, {
         status: 200,
     });
 }
