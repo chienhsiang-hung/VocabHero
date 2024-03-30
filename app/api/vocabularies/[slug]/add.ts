d8aa18dbcc188424be65065402bb1aedddca40c1
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2024-03-30 21:06:57 +0800

    fixed Postgres Case Sensitivity issue

--- app/api/vocabularies/[slug]/add.ts
+++ app/api/vocabularies/[slug]/add.ts
@@ -1,5 +1,12 @@
 import { sql } from "@vercel/postgres";
 
 export default async function add() {
-    console.log('hi');
+    await sql`
+    INSERT INTO vocabularies
+    SELECT "userId", 'apple', 'http...', FALSE, 'meaning'
+    FROM "Session"
+    WHERE "sessionToken" = '3e37fee9-d2b4-47c1-9cf8-a729cb6ebbfb';
+    `;
+
+    console.log('ok');
 }
