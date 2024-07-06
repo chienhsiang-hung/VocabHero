2be4d62ddde74fe91c24c485c4107ce2352dec85
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2024-07-06 17:40:49 +0800

    callback for user.id still needs to be added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,6 +11,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  // callback for user.id still needs to be added
 };
 
 const handler = NextAuth(authOptions);
